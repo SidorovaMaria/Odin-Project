5477a4ab419f5dc6f84c9e532b1d6198582264cb
Maria Sidorova <sidmashav@icloud.com>
2025-09-08 15:45:11 +0100

    Inital Screen Render and Style

---
+++ JavaScript/SonarStrike/src/DOM/main.js
@@ -0,0 +1,24 @@
+import { StartScreen } from "./screens/startScreen";
+
+// c app() {
+//     const appElement = document.getElementById("app");
+//     function loadGame() {
+//         //Reset all app content
+//         appElement.replaceChildren("");
+//         const startScreen = new StartScreen(appElement);
+//         startScreen.createStartCard(appElement);
+//     }
+//     // You may want to expose loadGame or call it here
+// }
+
+export class App {
+    constructor() {
+        this.root = document.getElementById("app");
+    }
+    loadGame() {
+        //Reset all app content
+        this.root.replaceChildren("");
+        const startScreen = new StartScreen(this.root);
+        startScreen.createStartCard(this.root);
+    }
+}
---
+++ JavaScript/SonarStrike/src/DOM/screens/startScreen.js
@@ -0,0 +1,106 @@
+import "./Screenstyle.css";
+import { create } from "../../utils/utils";
+export class StartScreen {
+    constructor(root) {
+        this.root = root;
+        this.conatiner = create("section", {
+            classes: ["start-screen"],
+            attrs: {
+                id: "start-screen",
+            },
+        });
+        this._bound = false;
+    }
+    createStartCard() {
+        this.createHeader();
+        this.createFormInput();
+        this.creaatePlayButton();
+        this.createAboutMe();
+        this.bindEvents();
+        //to be implemented
+        this.root.appendChild(this.conatiner);
+    }
+    createHeader() {
+        const header = create("div", { classes: ["header-container"] });
+        const IconContainer = create("div", { classes: ["icon-container"] });
+        const icon = create("i", { classes: ["fa-solid", "fa-ship"] });
+        IconContainer.appendChild(icon);
+        const title = create("h2", { text: "Sonar Strike" });
+        const description = create("p", {
+            text: "Classfied mission - Sonar Strike",
+        });
+        header.append(IconContainer, title, description);
+        this.conatiner.appendChild(header);
+    }
+    createFormInput() {
+        const form = create("form", { classes: ["start-form"], attrs: { id: "player-name-form" } });
+        const label = create("label", {
+            text: "Commander Callsign",
+            attrs: { for: "player-name" },
+        });
+        const input = create("input", {
+            classes: ["input-field"],
+            attrs: {
+                type: "text",
+                id: "player-name",
+                name: "player-name",
+                placeholder: "Enter your callsign...",
+            },
+        });
+        form.append(label, input);
+        this.conatiner.appendChild(form);
+    }
+    creaatePlayButton() {
+        const button = create("button", {
+            classes: ["play-button"],
+            text: "Initiate Mission",
+            attrs: { type: "submit", form: "player-name-form", disabled: true },
+        });
+        const icon = create("i", { classes: ["fa-solid", "fa-play", "fa-mp3"] });
+        button.prepend(icon);
+        this.conatiner.appendChild(button);
+    }
+    createAboutMe() {
+        const aboutMe = create("div", { classes: ["about-me"] });
+        const github = create("button", { classes: ["github-link", "link-icon"] });
+        const icon = create("i", { classes: ["fa-brands", "fa-github"] });
+        const githubLink = create("a", {
+            attrs: { href: "https://github.com/SidorovaMaria", target: "_blank" },
+        });
+        const githubSpan = create("span", {
+            attrs: {
+                id: "github-span",
+            },
+        });
+        githubLink.append(icon);
+        github.append(githubLink, githubSpan);
+        const portfolio = create("button", { classes: ["portfolio-link", "link-icon"] });
+        const portfolioIcon = create("i", { classes: ["fa-solid", "fa-globe"] });
+        const portfolioLink = create("a", {
+            attrs: { href: "https://portfolio-6dft.vercel.app/", target: "_blank" },
+        });
+        const portfolioSpan = create("span", {
+            attrs: { id: "portfolio-span" },
+        });
+        portfolioLink.append(portfolioIcon);
+        portfolio.append(portfolioLink, portfolioSpan);
+        aboutMe.append(github, portfolio);
+        this.conatiner.appendChild(aboutMe);
+    }
+    remove() {
+        this.conatiner.remove();
+    }
+    bindEvents() {
+        if (this._bound) return;
+        const inputField = this.conatiner.querySelector("#player-name");
+        const playButton = this.conatiner.querySelector(".play-button");
+        inputField.addEventListener("input", () => {
+            if (inputField.value.trim().length > 0) {
+                playButton.disabled = false;
+            } else {
+                playButton.disabled = true;
+            }
+        });
+        this._bound = true;
+    }
+}
